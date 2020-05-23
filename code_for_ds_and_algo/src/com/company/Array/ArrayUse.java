53e0499b3b07657f7416b537b178ad7d4e38ea37
prabhaker24 <prabhaker.saxena@dell.com>
2020-05-23 21:24:31 +0530

    first array use program added
    Signed-off-by: prabhaker24 <prabhaker.saxena@dell.com>

---
+++ code_for_ds_and_algo/src/com/company/Array/ArrayUse.java
@@ -0,0 +1,13 @@
+package com.company.Array;
+
+public class ArrayUse {
+
+    public static void main(String[] args) {
+        // write your code here
+        int arr[]=new int[10];
+        for(int i =0;i<10;i++)
+            arr[i]=i+1;
+        for(int i =0;i<10;i++)
+            System.out.println(arr[i]);
+    }
+}
